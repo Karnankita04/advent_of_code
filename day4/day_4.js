972874d18ef2f34a2731ebac3588d2e38d101485
Karnankita04 <91003709+Karnankita04@users.noreply.github.com>
2025-02-02 16:26:06 +0530

    done with part1 with little bit cleanliness

--- day4/day_4.js
+++ day4/day_4.js
@@ -1,63 +1,28 @@
-const areAdjacentDigitsSame = (number) => {
-  let num = number;
-
-  while (num) {
-    const lastDigit = num % 10;
-    num = Math.floor(num / 10);
-
-    if (lastDigit === num % 10) {
-      return true;
-    }
-  }
-
-  return false;
+const areAdjacentDigitsSame = (array) => {
+  return array.some((num, index) => {
+    if (index === array.length - 1) return false;
+    return num === array[index + 1];
+  });
 };
 
-const areAllDigitsSame = (number) => {
-  let num = number;
-
-  while (num) {
-    const lastDigit = num % 10;
-    num = Math.floor(num / 10);
-
-    if (lastDigit !== num % 10) {
-      return false;
-    }
-  }
-
-  return true;
+const areInIncreasingOrder = (array) => {
+  return array.every((number, index) => {
+    if (index === array.length - 1) return true;
+    return number <= array[index + 1];
+  });
 };
-
-const areInIncreasingOrder = (number) => {
-  let num = number;
-
-  while (num) {
-    const lastDigit = num % 10;
-    num = Math.floor(num / 10);
-
-    if (lastDigit < num % 10) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-// use of some , every
 
 const main = (range1, range2) => {
   let count = 0;
 
   for (let input = range1; input <= range2; input++) {
-    if (
-      (areAdjacentDigitsSame(input) && areInIncreasingOrder(input)) ||
-      areAllDigitsSame(input)
-    ) {
-      console.log(input);
+    const arr = (input + "").split("");
+
+    if (areAdjacentDigitsSame(arr) && areInIncreasingOrder(arr)) {
       count++;
     }
   }
-
+  
   return count;
 };
 
